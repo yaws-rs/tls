8cc21824a8f119896f482f125d6c1c701619721a
pinkforest <36498018+pinkforest@users.noreply.github.com>
2025-09-01 20:05:33 +0000

    Clean example a bit

--- blueprint/examples/tick-tock.rs
+++ blueprint/examples/tick-tock.rs
@@ -4,6 +4,7 @@ use blueprint::{BluePrint, Orbit};
 use blueprint::{Left, Right};
 use blueprint_tls::{TlsClient, TlsServer};
 use blueprint_tls::{TlsClientConfig, TlsServerConfig};
+use blueprint_tls::TlsPosition;
 
 const CA: &'static str = "certs/ca.rsa4096.crt";
 const CERT: &'static str = "certs/rustcryp.to.rsa4096.ca_signed.crt";
@@ -11,45 +12,51 @@ const KEY: &'static str = "certs/rustcryp.to.rsa4096.key";
 
 use std::path::Path;
 
-struct ClientPreTls {
+struct PreTls {
     buf_in: Vec<u8>,
     buf_in_pos: usize,
     buf_out: Vec<u8>,
 }
 
-struct ServerPreTls {
-    buf_in: Vec<u8>,
-    buf_in_pos: usize,
-    buf_out: Vec<u8>,
-}
-
-impl Left for ClientPreTls {
+impl Left for PreTls {
     fn bufs(&mut self) -> (&mut [u8], &mut [u8]) {
         (&mut self.buf_in[0..self.buf_in_pos], &mut self.buf_out)
     }
 }
 
-impl Left for ServerPreTls {
-    fn bufs(&mut self) -> (&mut [u8], &mut [u8]) {
-        (&mut self.buf_in[0..self.buf_in_pos], &mut self.buf_out)
+struct PostTls {
+    buf_in: Vec<u8>,
+    buf_in_pos: usize,
+    buf_out: Vec<u8>,
+}
+
+impl Right for PostTls {}
+
+fn aft_emulate_network<'a>(tls_pos: &TlsPosition, tx: &mut PreTls, rx: &mut PreTls) {
+    let zero_buf = vec![0u8; 8192];    
+
+    if tls_pos.out_send > 0 {
+        let out_bytes = tls_pos.out_send;
+        println!("Sending = {}", out_bytes);
+        let out_end = rx.buf_in_pos + out_bytes;
+        rx.buf_in[rx.buf_in_pos..out_end]
+            .copy_from_slice(&tx.buf_out[0..out_bytes]);
+        rx.buf_in_pos += out_bytes;
+        tx.buf_out = vec![0u8; 8192];
     }
+
+    if tls_pos.in_discard > 0 {
+        let discard = tls_pos.in_discard;
+        let copy_count = 8192 - discard;
+                tx.buf_in.rotate_left(discard);
+        tx.buf_in[discard..8192].copy_from_slice(&zero_buf[discard..8192]);
+        tx.buf_in_pos -= discard;
+        println!(
+            "Discarded {} pos after = {}",
+            discard, tx.buf_in_pos
+        );
+    }            
 }
-
-struct ClientPostTls {
-    buf_in: Vec<u8>,
-    buf_in_pos: usize,
-    buf_out: Vec<u8>,
-}
-
-struct ServerPostTls {
-    buf_in: Vec<u8>,
-    buf_in_pos: usize,
-    buf_out: Vec<u8>,
-}
-
-impl Right for ClientPostTls {}
-
-impl Right for ServerPostTls {}
 
 fn main() {
     let config_server =
@@ -60,22 +67,22 @@ fn main() {
     let mut server = TlsServer::with_config(config_server).unwrap();
     let mut client = TlsClient::with_config(config_client).unwrap();
 
-    let mut client_left = ClientPreTls {
+    let mut client_left = PreTls {
         buf_in: vec![0u8; 8192],
         buf_in_pos: 0,
         buf_out: vec![0u8; 8192],
     };
-    let mut client_right = ClientPostTls {
+    let mut client_right = PostTls {
         buf_in: vec![0u8; 8192],
         buf_in_pos: 0,
         buf_out: vec![0u8; 8192],
     };
-    let mut server_left = ServerPreTls {
+    let mut server_left = PreTls {
         buf_in: vec![0u8; 8192],
         buf_in_pos: 0,
         buf_out: vec![0u8; 8192],
     };
-    let mut server_right = ServerPostTls {
+    let mut server_right = PostTls {
         buf_in: vec![0u8; 8192],
         buf_in_pos: 0,
         buf_out: vec![0u8; 8192],
@@ -91,29 +98,9 @@ fn main() {
             .advance_with(&mut u, &mut client_left, &mut client_right)
             .unwrap();
         println!("TLS Client Pos = {:?}", tls_client_pos);
-        // Assume this was sent over network Client -> Server
-        if tls_client_pos.out_send > 0 {
-            let out_bytes = tls_client_pos.out_send;
-            println!("Client sending = {}", out_bytes);
-            let out_end = server_left.buf_in_pos + out_bytes;
-            server_left.buf_in[server_left.buf_in_pos..out_end]
-                .copy_from_slice(&client_left.buf_out[0..out_bytes]);
-            server_left.buf_in_pos += out_bytes;
-            client_left.buf_out = vec![0u8; 8192];
-        }
-
-        if tls_client_pos.in_discard > 0 {
-            let discard = tls_client_pos.in_discard;
-            let copy_count = 8192 - discard;
-            client_left.buf_in.rotate_left(discard);
-            client_left.buf_in[discard..8192].copy_from_slice(&zero_buf[discard..8192]);
-            client_left.buf_in_pos -= discard;
-            println!(
-                "Client discarded {} pos after = {}",
-                discard, client_left.buf_in_pos
-            );
-        }
-
+        // Assume this was sent over network Client -> Server        
+        aft_emulate_network(&tls_client_pos, &mut client_left, &mut server_left);
+        
         println!(
             "-- Server Pos client_in = {}, server_in = {}",
             client_left.buf_in_pos, server_left.buf_in_pos
@@ -126,27 +113,7 @@ fn main() {
                 .unwrap();
             println!("TLS Server Pos = {:?}", tls_server_pos);
             // Assume this was sent over network Server -> Client
-            if tls_server_pos.out_send > 0 {
-                let out_bytes = tls_server_pos.out_send;
-                println!("Server sending = {}", out_bytes);
-                let out_end = client_left.buf_in_pos + out_bytes;
-                client_left.buf_in[client_left.buf_in_pos..out_bytes]
-                    .copy_from_slice(&server_left.buf_out[0..out_bytes]);
-                client_left.buf_in_pos += out_bytes;
-                server_left.buf_out = vec![0u8; 8192];
-            }
-
-            if tls_server_pos.in_discard > 0 {
-                let discard = tls_server_pos.in_discard;
-                let copy_count = 8192 - discard;
-                server_left.buf_in.rotate_left(discard);
-                server_left.buf_in[discard..8192].copy_from_slice(&zero_buf[discard..8192]);
-                server_left.buf_in_pos -= discard;
-                println!(
-                    "Server discarded {} pos after = {}",
-                    discard, server_left.buf_in_pos
-                );
-            }
+            aft_emulate_network(&tls_server_pos, &mut server_left, &mut client_left);
         }
     }
 }
